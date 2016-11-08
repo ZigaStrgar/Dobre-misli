18df25e87d36dbc79112903842720244b119b38b
ZigaStrgar <ziga_strgar@hotmail.com>
2016-11-08 10:29:33 +0100

    Does it work now?:+1:

--- Dobre misli/Default.aspx.cs
+++ Dobre misli/Default.aspx.cs
@@ -9,14 +9,25 @@ namespace Dobre_misli
 {
     public partial class Default : System.Web.UI.Page
     {
+        public static List<Label> ToughLabels = new List<Label>();
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+            misel.Text = "";
         }
 
         protected void dodajMisel_Click(object sender, EventArgs e)
         {
-                    
+            Label l = new Label();
+            l.Text = "\""+Request.Form["misel"]+"\"";
+            l.Style.Add("display", "block");
+            ToughLabels.Add(l);
+            if (ToughLabels.Count > 0)
+            {
+                foreach (Label lab in ToughLabels)
+                {
+                    FindControl("misli").Controls.AddAt(0, lab);
+                }
+            }
         }
     }
     
